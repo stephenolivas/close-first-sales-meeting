a122c5845e6e6f7a85c721e66b555f54ab119a82
stephenolivas <stephen@modern-amenities.com>
2026-05-19 18:53:35 -0700

    Update update_field.py

--- update_field.py
+++ update_field.py
@@ -120,6 +120,7 @@ CLOSER_PATTERNS = [
     re.compile(r"post\s+masterclass\s+strategy\s+call", re.IGNORECASE),
     re.compile(r"vending\s+route\s+consultation", re.IGNORECASE),
     re.compile(r"cash[\-\s]flowing\s+vending\s+route\s+advisory\s+interview", re.IGNORECASE),
+    re.compile(r"vending\s+route\s+advisory\s+call", re.IGNORECASE),
 ]
 
 
